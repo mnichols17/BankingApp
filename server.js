79f33397e383bc7cc9f8c186fce3d7e78654f752
Mike <mike.nichols97@gmail.com>
2020-05-14 23:38:16 -0400

    Attempting to deploy to Heroku

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const express = require('express');
-
+const path = require('path');
 const app = express();
 const port = process.env.PORT || 5000;
 
@@ -7,5 +7,13 @@ app.use(express.json());
 
 app.use('/api/profiles', require('./routes/profiles'));
 app.use('/api/transactions', require('./routes/transactions'));
+
+if(process.env.NODE_ENV === "production"){
+    app.use(express.static('client/build'))
+
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+    })
+}
 
 app.listen(port, () => console.log(`Server started on ${port}`));
